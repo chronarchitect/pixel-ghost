cb3c11ff378a45f62baf84e6587e2392d5e26f3d
Aniket Das <aniketdas674@gmail.com>
2026-06-06 15:33:21 +0530

    deploy: update script to stop existing containers before redeploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Stop and remove existing containers defined in compose.prod.yaml
+docker-compose -f compose.prod.yaml down
+
 # Start the services
 DOMAIN_NAME='yourdomain.com' docker-compose -f compose.prod.yaml up -d --build
